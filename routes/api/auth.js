15a1186478776a3c007b9d252e4163e90573e584
AzureAD\VladimirShtofmakher <Vladimirs@gmedinnovations.com>
2019-05-31 18:37:51 +0300

    profile api routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -61,7 +61,7 @@ router.post('/', [
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        {expiresIn: 360000},
+        {expiresIn: 43200},
         (err, token) => {
           if (err) throw err;
           res.json({token});
