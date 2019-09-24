346469a6ac6a0ffe30ee0320f9e8e321ae57d70e
AzureAD\VladimirShtofmakher <Vladimirs@gmedinnovations.com>
2019-09-24 17:10:17 +0300

    Setting up for deploying to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -11,12 +12,23 @@ connectDB();
 // (Parse incoming request bodies in a middleware before your handlers, available under the req.body property)
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => res.send('API running'));
 // Define routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  //  Set static folder
+  //  express will serve up production assets like our main.js, or main.css file
+  app.use(express.static('client/build'));
+
+  //  express will serve up the index.html file if it does not recognize the route
+ app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+ });
+}
 
 const PORT = process.env.PORT || 5000;
 
